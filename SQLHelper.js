648b63dd8716bcc2b5f5a0e1243f20d7f8a1ca58
dim0nickb <basstorniloo@gmail.com>
2015-06-28 21:53:20 +0300

    Some functions to work with SQL: AddUser

---
+++ SQLHelper.js
@@ -0,0 +1,32 @@
+﻿var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host     : '173.194.86.81',
+	user     : 'root',
+	password : 'siskipiski',
+	database : 'gsac'
+});
+
+function exec(query) {
+	connection.connect();
+	connection.query(query, function (err, rows, fields) {
+		if (!err) {
+			return rows;
+		}
+		else
+			console.log('Error while performing Query.');
+	});
+	connection.end();
+}
+function isNumber(obj) { return !isNaN(parseFloat(obj)) }
+
+function addUser(userID) {
+	if (!isNumber(userID))
+		return false;
+
+	var q = 'SELECT * FROM users WHERE userID = ' + userID;
+	if (exec(q).length === 0) {
+		q = 'INSERT INTO users (userID, total, score) VALUES (' + userID + '0, 0';
+		exec(q);
+	}
+	return true;
+}
--- server.js
+++ server.js
@@ -2,27 +2,39 @@ var http = require("http");
 var url = require("url");
 
 function start(route, handle) {
-  function onRequest(request, response) {
-    var postData = "";
-    var pathname = url.parse(request.url).pathname;
-//    console.log("Request for " + pathname + " received.");
+	function onRequest(request, response) {
+		
+		//пробуем зарегистрировать пользователя.
+		var url_parts = url.parse(request.url, true);
+		var query = url_parts.query;
+		// получаем viewer_id из полученных переменных
+		var viewer_id = query['viewer_id'];
+		// выполняем запрос получения профиля
+		var sqlHelper = require('./SQLHelper.js');
+		if (!sqlHelper.addUser(viewer_id)) {
+			response.statusCode = 500;
+			response.end("Bad user! Please contact to <a href = 'http://vk.com/drgonza'>developer</a>");
+		}
 
-    request.setEncoding("utf8");
+		var postData = "";
+		var pathname = url.parse(request.url).pathname;
 
-    request.addListener("data", function(postDataChunk) {
-      postData += postDataChunk;
-      console.log("Received POST data chunk '"+
-      postDataChunk + "'.");
-    });
+		request.setEncoding("utf8");
 
-    request.addListener("end", function() {
-      route(handle, pathname, response, postData);
-    });
+		request.addListener("data", function(postDataChunk) {
+			postData += postDataChunk;
+			console.log("Received POST data chunk '"+
+			postDataChunk + "'.");
+		});
 
-  }
+		request.addListener("end", function() {
+			route(handle, pathname, response, postData);
+		});
 
-  http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+	}
+
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started @8888");
 }
 
 exports.start = start;
