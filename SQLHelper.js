cf8c85bbf067d92608634fbf3f293d852b58e384
dim0nickb <basstorniloo@gmail.com>
2015-06-30 07:47:35 +0300

    use callbacks

--- SQLHelper.js
+++ SQLHelper.js
@@ -14,26 +14,24 @@ var pool = mysql.createPool({
 	database : 'gsac'
 });
 
-function exec(query){
+function exec(q, callback){
 	var res = [];
-	pool.getConnection(function (err, connection) {
-		// Use the connection
-		connection.query(query, function (err, rows) {
-			if (!err) {
-				console.log(rows);
-				res = rows;
-				return res;
-			}
-			else {
-				console.log('Error while performing Query.');
-				return res;
-			}
-			// And done with the connection.
+	pool.getConnection(function (err, connection, callback) {
+			connection.query(q, onExec);
 			connection.release();
-		// Don't use the connection here, it has been returned to the pool.
 		});
-	});
 }
+
+function onExec(err, rows) {
+	if (!err) {
+		console.log(rows);
+		callback(rows);
+	}
+	else {
+		console.log('Error while performing Query.');
+	}
+};
+
 //function exec(query) {
 //	var res = [];
 //	connection.connect();
@@ -48,22 +46,31 @@ function exec(query){
 //	connection.end();
 //	return res;
 //}
+
 function isNumber(obj) { return !isNaN(parseFloat(obj)) }
 
-function addUser(userID) {
+function addUser(userID,  callback) {
 	if (!isNumber(userID))
 		return false;
 
 	var q = 'SELECT * FROM users WHERE userID = ' + userID + ';';
 	console.log(q);
-	var res = exec(q);
-	console.log(res);
-	if (res.length === 0) {
-		q = 'INSERT INTO users (userID, total, score) VALUES (' + userID + ', 0, 0);';
-		console.log(q);
-		exec(q);
+	exec(q, insertNewUser);
+
+	function insertNewUser(res) {
+		console.log(res);
+		if (res.length === 0) {
+			q = 'INSERT INTO users (userID, total, score) VALUES (' + userID + ', 0, 0);';
+			console.log(q);
+			exec(q, done);
+		}
+		else
+			callback(false);
 	}
-	return true;
+
+	function done(res) {
+		callback(true);
+	}
 }
 
 exports.addUser = addUser;
--- server.js
+++ server.js
@@ -11,10 +11,10 @@ function start(route, handle) {
 		var viewer_id = query['viewer_id'];
 		// выполняем запрос получения профиля
 		var sqlHelper = require('./SQLHelper.js');
-		if (!sqlHelper.addUser(viewer_id)) {
-			response.statusCode = 500;
-			var s = 
-				'<html>' +
+		sqlHelper.addUser(viewer_id, function (res) {
+			if (!res) {
+				response.statusCode = 500;
+				var s = '<html>' +
 				'<head>' +
 				'<meta http-equiv="Content-Type" content="text/html; ' +
 				'charset=UTF-8" />' +
@@ -23,27 +23,28 @@ function start(route, handle) {
 				'<div id = "badUser">Bad user !<BR>Please contact to <a href = "http://vk.com/drgonza">developer</a></div>' +
 				'</body>' +
 				'</html>';
-			response.writeHead(200, { "Content-Type": "text/html" });
-			response.write(s);
-			response.end();
-			return;
-		}
+				response.writeHead(200, { "Content-Type": "text/html" });
+				response.write(s);
+				response.end();
+				return;
+			}
+			else {
+				var postData = "";
+				var pathname = url.parse(request.url).pathname;
+				
+				request.setEncoding("utf8");
+				
+				request.addListener("data", function (postDataChunk) {
+					postData += postDataChunk;
+					console.log("Received POST data chunk '" + postDataChunk + "'.");
+				});
+				
+				request.addListener("end", function () {
+					route(handle, pathname, response, postData);
+				});
+			}
 
-		var postData = "";
-		var pathname = url.parse(request.url).pathname;
-
-		request.setEncoding("utf8");
-
-		request.addListener("data", function(postDataChunk) {
-			postData += postDataChunk;
-			console.log("Received POST data chunk '"+
-			postDataChunk + "'.");
 		});
-
-		request.addListener("end", function() {
-			route(handle, pathname, response, postData);
-		});
-
 	}
 
 	http.createServer(onRequest).listen(8888);
