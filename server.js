829b48bc2a43393b7f9b83215f7f5543c2e146a1
dim0nickb <basstorniloo@gmail.com>
2015-07-01 00:30:29 +0300

    ...

--- server.js
+++ server.js
@@ -28,22 +28,23 @@ function start(route, handle) {
 				response.end();
 				return;
 			}
+			else
+			{
+				var postData = "";
+				var pathname = url.parse(request.url).pathname;
+				
+				request.setEncoding("utf8");
+				/*
+				request.addListener("data", function (postDataChunk) {
+					postData += postDataChunk;
+					console.log("Received POST data chunk '" + postDataChunk + "'.");
+				});
+				/**/
+				request.addListener("end", function () {
+					route(handle, pathname, response, postData);
+				});
+			}
 		});
-
-		var postData = "";
-		var pathname = url.parse(request.url).pathname;
-		
-		request.setEncoding("utf8");
-		/*
-		request.addListener("data", function (postDataChunk) {
-			postData += postDataChunk;
-			console.log("Received POST data chunk '" + postDataChunk + "'.");
-		});
-		/**/
-		request.addListener("end", function () {
-			route(handle, pathname, response, postData);
-		});
-
 	}
 
 	http.createServer(onRequest).listen(8888);
