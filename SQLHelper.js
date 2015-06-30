0a6f7818159e506e2fb6519ade3d4e4dc0fbbf64
dim0nickb <basstorniloo@gmail.com>
2015-06-30 22:56:00 +0300

    ...

--- SQLHelper.js
+++ SQLHelper.js
@@ -13,10 +13,11 @@ function exec(q, callback){
 				if (!err) {
 					console.log('callback called with result: ');
 					console.log(rows);
-					cb(rows);
+					cb(rows, true);
 				}
 				else {
 					console.log('Error while performing Query.');
+					cb(rows, false);
 				}
 			});
 			connection.release();
@@ -26,6 +27,7 @@ function exec(q, callback){
 function isNumber(obj) { return !isNaN(parseFloat(obj)) }
 
 function addUser(userID,  callback) {
+	var cb = callback;
 	if (!isNumber(userID))
 		return false;
 
@@ -33,20 +35,22 @@ function addUser(userID,  callback) {
 	console.log(q);
 	exec(q, insertNewUser);
 
-	function insertNewUser(res) {
-		console.log(res);
-		console.log(res.length);
-		if (res.length === 0) {
+	function insertNewUser(rows, res) {
+		if (!res)
+			cb(false);
+		console.log(rows);
+		console.log(rows.length);
+		if (rows.length === 0) {
 			q = 'INSERT INTO users (userID, total, score) VALUES (' + userID + ', 0, 0);';
 			console.log(q);
 			exec(q, done);
 		}
 		else
-			callback(false);
+			cb(true);
 	}
 
-	function done(res) {
-		callback(true);
+	function done(rows, res) {
+		cb(res);
 	}
 
 }
